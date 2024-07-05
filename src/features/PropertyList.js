a72191fff19ce6fea0e76b03dc7633da6a428650
DamithaPerera <damitha@supplied.eu>
2024-07-05 16:17:19 +0530

    fix

--- src/features/PropertyList.js
+++ src/features/PropertyList.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { TextField, Grid, Typography, Card, CardContent } from '@material-ui/core';
-import { setFilterText } from './propertySlice';
+import {useSelector, useDispatch} from 'react-redux';
+import {TextField, Grid, Typography, Card, CardContent} from '@material-ui/core';
+import {setFilterText} from './propertySlice';
 
 function PropertyList() {
     const dispatch = useDispatch();
-    const { filteredList, filterText } = useSelector((state) => state.properties);
-
+    const {filteredList, filterText} = useSelector((state) => state.properties);
+    const [expandedProperty, setExpandedProperty] = React.useState(null);
+    const handleSeeMore = (id) => {
+        setExpandedProperty((prevExpandedProperty) => (prevExpandedProperty === id ? null : id));
+    };
     const handleFilterChange = (event) => {
         dispatch(setFilterText(event.target.value));
+        setExpandedProperty(null);
     };
 
     return (
-        <>
+        <React.Fragment>
             <TextField
                 fullWidth
                 variant="outlined"
                 label="Filter properties"
                 value={filterText}
                 onChange={handleFilterChange}
-                style={{ marginBottom: '20px' }}
+                style={{marginBottom: '20px'}}
             />
             {filteredList.length === 0 ? (
                 <Typography variant="h6">No properties found</Typography>
@@ -27,8 +31,8 @@ function PropertyList() {
                 <Grid container spacing={3}>
                     {filteredList.map((property) => (
                         <Grid item xs={12} sm={6} md={4} key={property.id}>
-                            <Card>
-                                <CardContent>
+                            <Card style={{height: '100%', display: 'flex', flexDirection: 'column'}}>
+                                <CardContent style={{flexGrow: 1}}>
                                     <Typography variant="h5" component="h2">
                                         {property.name}
                                     </Typography>
@@ -36,7 +40,22 @@ function PropertyList() {
                                         {property.code}
                                     </Typography>
                                     <Typography variant="body2" component="p">
-                                        {property.description}
+                                        {expandedProperty === property.id ? (
+                                            <React.Fragment>
+                                                {property.description}
+                                                <span style={{color: 'blue', cursor: 'pointer'}}
+                                                      onClick={() => handleSeeMore(property.id)}>see less</span>
+                                            </React.Fragment>
+                                        ) : (
+                                            <React.Fragment>
+                                                {property.description.substring(0, 165)}
+                                                {property.description.length > 165 && '...'}
+                                                {property.description.length > 165 && (
+                                                    <span style={{color: 'blue', cursor: 'pointer'}}
+                                                          onClick={() => handleSeeMore(property.id)}>see more</span>
+                                                )}
+                                            </React.Fragment>
+                                        )}
                                     </Typography>
                                 </CardContent>
                             </Card>
@@ -44,7 +63,7 @@ function PropertyList() {
                     ))}
                 </Grid>
             )}
-        </>
+        </React.Fragment>
     );
 }
 
